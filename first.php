9e0adf5e0e7dc7116772738bc9024abe10fca0ed
karlkruu <karlkruu@greeny.cs.tlu.ee>
2016-09-06 15:17:13 +0300

    tunnis notepad++

---
+++ first.php
@@ -0,0 +1,18 @@
+tere
+
+<?php
+
+	$firstName = "Karl";
+	$lastName = "Kruus";
+
+	// echo trükib välja muutuja veebilehel
+	
+	echo $firstName;
+	echo " ";
+	echo $lastName;
+	
+	// punkt liidab stringe
+	
+	echo $firstName." ".$lastName;
+	
+?>
